c72d9ecc0ec5ddc2749a8f154bef5460d5c7bc0a
nottiffchan <nottiffchan@gmail.com>
2022-10-19 18:28:14 +0800

    Add react-animated-cursor package

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import "./App.css";
-
 import ProjectPage from "./ProjectPage";
+import AnimatedCursor from "react-animated-cursor";
 
 function App() {
-  return <ProjectPage pageId="d0ffd99c246b418db93a9c19b2943cf6" />;
+  return (
+    <div className="App" style={{ height: "100vh" }}>
+      <AnimatedCursor color="249, 144, 144" />
+      <ProjectPage pageId="d0ffd99c246b418db93a9c19b2943cf6" />;
+    </div>
+  );
 }
 
 export default App;
