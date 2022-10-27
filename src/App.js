a292068680a75526f1a00931a5be77db64940e09
nottiffchan <nottiffchan@gmail.com>
2022-10-27 12:10:02 +0800

    Add Vercel analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 import NotionPage from "./pages/NotionPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { WavyContainer } from "react-wavy-transitions";
@@ -47,6 +48,7 @@ function App() {
       )}
 
       <Footer />
+      <Analytics />
     </div>
   );
 }
